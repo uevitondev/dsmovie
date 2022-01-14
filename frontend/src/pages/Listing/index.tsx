c37d579c932ad88ee785272023722bb4c1008343
uevitondev <uevitonsantos@outlook.com>
2022-01-14 18:16:36 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -7,7 +7,6 @@ import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-
     const [pageNumber, setPageNumber] = useState(0);
 
     const [page, setPage] = useState<MoviePage>({
@@ -31,14 +30,18 @@ function Listing() {
             });
     }, [pageNumber]);
 
+    const handlePageChange = (newPageNumber: number) => {
+        setPageNumber(newPageNumber);
+    }
+
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
 
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
-                        <div  key={movie.id} className="col-sm-6 col-md-5 col-lg-4 col xl-3 col-xxl-2 mb-3">
+                        <div key={movie.id} className="col-sm-6 col-md-5 col-lg-4 col xl-3 col-xxl-2 mb-3">
                             <MovieCard movie={movie} />
                         </div>
                     )
